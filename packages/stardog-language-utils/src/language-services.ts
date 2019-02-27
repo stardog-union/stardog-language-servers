dd9edd4f1acb3b12963dc355eb4831cb4ac5094f
Jason Rogers <jason@stardog.com>
2019-02-27 14:11:42 -0500

    Handle autocomplete in cases with longer alternatives (close #10)

--- packages/stardog-language-utils/src/language-services.ts
+++ packages/stardog-language-utils/src/language-services.ts
@@ -1,25 +1,31 @@
-import { IToken } from "millan";
-import uniq from "lodash.uniq";
+import { IToken } from 'millan';
+import uniqBy from 'lodash.uniqby';
 
 export const regexPatternToString = (pattern: RegExp | string) =>
   pattern
     .toString()
-    .split("/")[1]
+    .split('/')[1]
     .toLowerCase()
-    .replace(" +", " "); // this may be flakey. the sparql phrases "Insert Data", "Delete Where", etc. have a "+" in their regexps in millan
+    .replace(' +', ' '); // this may be flakey. the sparql phrases "Insert Data", "Delete Where", etc. have a "+" in their regexps in millan
 
-export const makeFilterUniqueTokensByType = (typeTester: (tokenName: string) => boolean) => (
-  tokens: IToken[]
-) => uniq(tokens.filter(tkn => typeTester(tkn.tokenType.tokenName)).map(tkn => tkn.image));
+export const makeFilterUniqueTokensByType = (
+  typeTester: (tokenName: string) => boolean
+) => (tokens: IToken[]) =>
+  uniqBy(
+    tokens
+      .filter((tkn) => typeTester(tkn.tokenType.tokenName))
+      .map((tkn) => tkn.image)
+  );
 
-export const isVar = (tokenName: string) => tokenName === "VAR1" || tokenName === "VAR2";
-export const isPrefix = (tokenName: string) => tokenName === "PNAME_NS";
-export const isLocalName = (tokenName: string) => tokenName === "PNAME_LN";
-export const isIriRef = (tokenName: string) => tokenName === "IRIREF";
+export const isVar = (tokenName: string) =>
+  tokenName === 'VAR1' || tokenName === 'VAR2';
+export const isPrefix = (tokenName: string) => tokenName === 'PNAME_NS';
+export const isLocalName = (tokenName: string) => tokenName === 'PNAME_LN';
+export const isIriRef = (tokenName: string) => tokenName === 'IRIREF';
 
 export const getUniqueIdentifiers = (tokens: IToken[]) => ({
   vars: makeFilterUniqueTokensByType(isVar)(tokens),
   prefixes: makeFilterUniqueTokensByType(isPrefix)(tokens),
   localNames: makeFilterUniqueTokensByType(isLocalName)(tokens),
-  iris: makeFilterUniqueTokensByType(isIriRef)(tokens)
+  iris: makeFilterUniqueTokensByType(isIriRef)(tokens),
 });
